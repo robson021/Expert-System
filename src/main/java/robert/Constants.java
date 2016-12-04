fefffa5e873ff162ebe0b7c87f40252ef94c5887
Robert <nowak021@gmail.com>
2016-12-04 16:09:59 +0100

    chane input values count

--- src/main/java/robert/Constants.java
+++ src/main/java/robert/Constants.java
@@ -2,22 +2,22 @@ package robert;
 
 public interface Constants {
 	String[] LABEL_NAMES = new String[]{
-			"Distance of route (km)",
-			"Crew experience (years)",
-			"Available average flight speed (km/h)",
-			"Weather condition (poor/normal/good)",
-			"Airport preparation, runway condition (poor/normal/good)",
-			"Number of passengers (latecomer probability)"
-	};
+            "Distance of route (0 - 5000km)",
+            //"Crew experience (years)",
+            "Available average flight speed (100 - 1000km/h)",
+            "Weather condition (-2.0  -  2.0)"
+            //"Airport preparation, runway condition (poor/normal/good)",
+            //"Number of passengers (latecomer probability)"
+    };
 
 	String[] CLP_VARIABLES = new String[]{
 			"(defglobal ?*distance* = %s )",
-			"(defglobal ?*crew* = %s )",
-			"(defglobal ?*speed* = %s )",
-			"(defglobal ?*weather* = %s )",
-			"(defglobal ?*airport* = %s )",
-			"(defglobal ?*passengers* = %s )"
-	};
+            //"(defglobal ?*crew* = %s )",
+            "(defglobal ?*speed* = %s )",
+            "(defglobal ?*weather* = %s )"
+            //"(defglobal ?*airport* = %s )",
+            //"(defglobal ?*passengers* = %s )"
+    };
 
 	short DEFAULT_TEXT_FIELD_WIDTH = 7;
 
